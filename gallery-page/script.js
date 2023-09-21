266d22c91461dc76e19059c6426ebafa6b3c8c30
Rajiv Kumar <mahirjv@gmail.com>
2023-09-21 09:34:56 +0530

    gallery page updated

---
+++ gallery-page/script.js
@@ -0,0 +1,36 @@
+// IMAGES PART
+$(function () {
+
+    $(".firstrow-img").animate({
+        "margin-left": "50%"
+    },3000);
+
+    $(".secondrow-img").animate({
+        "margin-right": "50%"
+    },3000);
+
+    $(".thirdrow-img").animate({
+        "margin-left": "50%"
+    },3000);
+
+    $(".fourthrow-img").animate({
+        "margin-right": "50%"
+    },3000);
+
+    $(".fifthrow-img").animate({
+        "margin-left": "50%"
+    },3000);
+
+    $(".sixthrow-img").animate({
+        "margin-right": "50%"
+    },3000);
+
+    $(".seventhrow-img").animate({
+        "margin-left": "50%"
+    },3000);
+
+    $(".eighthrow-img").animate({
+        "margin-right": "50%"
+    },3000);
+
+});
